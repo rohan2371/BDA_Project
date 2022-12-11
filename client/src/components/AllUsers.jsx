edcd95358a9087bf0b96153b58a78c97cf2e6307
Rohan <rohankonkatti2371@gmail.com>
2022-12-11 18:54:26 +0530

    commit 6

--- client/src/components/AllUsers.jsx
+++ client/src/components/AllUsers.jsx
@@ -1,7 +1,18 @@
 
 
+import {Table, TableBody, TableHead} from '@mui/material';
 const AllUser = ()=>{
-    return(<></>);
+    return(
+        <Table>
+            <TableHead>
+
+            </TableHead>
+            
+            <TableBody>
+
+            </TableBody>
+        </Table>
+    );
 }
 
 export default AllUser;
